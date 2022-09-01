30e957f61ae595828bcec74d0975bf5bf97215e6
maikelordaz <maikelordaz@gmail.com>
2022-08-31 22:32:22 -0400

    adjusting from moralis to the graph

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
                 <meta name="description" content="NFT Marketplace" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+            <MoralisProvider initializeOnMount={false}>
                 <NotificationProvider>
                     <Header />
                     <Component {...pageProps} />
