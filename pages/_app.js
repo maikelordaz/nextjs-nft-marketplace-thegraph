40b0578d38694283ed6ccfcf3072d01857643e9b
maikelordaz <maikelordaz@gmail.com>
2022-09-02 12:46:54 -0400

    Final touches and checks

--- pages/_app.js
+++ pages/_app.js
@@ -5,26 +5,10 @@ import Head from "next/head"
 import { NotificationProvider } from "web3uikit"
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 
-//<MoralisProvider initializeOnMount={false}>
-//<MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-/*
- * Ya no necesitare esto ya que lo voy a inicializar con mi Apollo Provider y GraphQl
- * const APP_ID = process.env.NEXT_PUBLIC_APP_ID
- * const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
- * Tengo que importar
- * import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
- * NEXT_PUBLIC_SUBGRAPH_URI lo tomo de mi dashboard -> details -> DEVELOPMENT QUERY URL
- * y hago lo siguiente:
- */
-
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     uri: process.env.NEXT_PUBLIC_SUBGRAPH_URI,
 })
-/*
- * Luego dentro de mi MoralisProvider pero afuera de mi NotificationProvider coloco los tags
- * de ApolloProvider
- */
 
 function MyApp({ Component, pageProps }) {
     return (
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-// Si quiero subir la pagina, debo quitar la Image tag porque complica todo con IPFS
 import styles from "../styles/Home.module.css"
 import { useMoralis } from "react-moralis"
 import NFTBox from "../components/NFTBox"
@@ -8,22 +7,11 @@ import GET_ACTIVE_ITEMS from "../constants/subgraphQueries"
 
 export default function Home() {
     const { isWeb3Enabled, chainId } = useMoralis()
-    /* No necesitare esto porque esto esta buscando en Moralis
-    const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
-        "ActiveItem",
-        (query) => query.limit(10).descending("tokenId")
-    )
-    */
     const chainString = chainId ? parseInt(chainId).toString() : "31337"
     const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
 
     const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS)
 
-    /* Ahora cambio los fetchingListedNfts por loading || !listedNfts es decir esta loading o no
-     * hay Nfts listeados. Y en vez de listedNfts.map((nft) va a ser
-     * listedNfts.activeItems.map((nft). Tampoco va a retornar con atributos como en
-     * nft.attributes sera solo nft
-     */
     return (
         <div className="container mx-auto">
             <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
